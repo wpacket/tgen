80634c8430d919c1bbd83d78f4ddbb1e577b4783
wpacket <40580581+wpacket@users.noreply.github.com>
2022-10-07 11:24:48 +0200

    Update tgen-web.sh

--- tgen-web.sh
+++ tgen-web.sh
@@ -3,30 +3,30 @@
 while true
 do
 
-wget -O /dev/null -q https://www.microsoft.com/fr-fr/microsoft-teams/log-in &
-wget -O /dev/null -q https://www.microsoft.com/en-us/microsoft-365/sharepoint/collaboration &
-wget -O /dev/null -q https://www.office.com/ &
-wget -O /dev/null -q https://login.salesforce.com/?locale=eu &
-wget -O /dev/null -q https://zoom.us/signin#/login &
-wget -O /dev/null -q https://web.webex.com/sign-in &
+wget -O /dev/null -q https://www.microsoft.com/fr-fr/microsoft-teams/log-in --no-check-certificate &
+wget -O /dev/null -q https://www.microsoft.com/en-us/microsoft-365/sharepoint/collaboration --no-check-certificate &
+wget -O /dev/null -q https://www.office.com/ --no-check-certificate &
+wget -O /dev/null -q https://login.salesforce.com/?locale=eu --no-check-certificate &
+wget -O /dev/null -q https://zoom.us/signin#/login --no-check-certificate &
+wget -O /dev/null -q https://web.webex.com/sign-in --no-check-certificate &
 
-wget -O /dev/null -q https://www.facebook.com/ &
-wget -O /dev/null -q https://www.instagram.com/ &
+wget -O /dev/null -q https://www.facebook.com/ --no-check-certificate &
+wget -O /dev/null -q https://www.instagram.com/ --no-check-certificate &
 
-wget -O /dev/null -q https://www.google.fr/maps &
-wget -O /dev/null -q https://drive.google.com/drive/my-drive &
-wget -O /dev/null -q https://mail.google.com/ &
+wget -O /dev/null -q https://www.google.fr/maps --no-check-certificate &
+wget -O /dev/null -q https://drive.google.com/drive/my-drive --no-check-certificate &
+wget -O /dev/null -q https://mail.google.com/ --no-check-certificate &
 
-wget -O /dev/null -q https://www.youtube.com/ &
-wget -O /dev/null -q https://open.spotify.com/ &
+wget -O /dev/null -q https://www.youtube.com/ --no-check-certificate &
+wget -O /dev/null -q https://open.spotify.com/ --no-check-certificate &
 
-wget -O /dev/null -q https://www.theguardian.com/ &
-wget -O /dev/null -q https://en.wikipedia.org/ &
+wget -O /dev/null -q https://www.theguardian.com/ --no-check-certificate &
+wget -O /dev/null -q https://en.wikipedia.org/ --no-check-certificate &
 
-wget -O /dev/null -q https://secure.eicar.org/eicar.com &
-wget -O /dev/null -q https://secure.eicar.org/eicar.com.txt &
-wget -O /dev/null -q https://secure.eicar.org/eicar_com.zip &
-wget -O /dev/null -q https://secure.eicar.org/eicarcom2.zip &
+wget -O /dev/null -q https://secure.eicar.org/eicar.com --no-check-certificate &
+wget -O /dev/null -q https://secure.eicar.org/eicar.com.txt --no-check-certificate &
+wget -O /dev/null -q https://secure.eicar.org/eicar_com.zip --no-check-certificate &
+wget -O /dev/null -q https://secure.eicar.org/eicarcom2.zip --no-check-certificate &
 
 sleep 3
 
