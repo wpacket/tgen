f4b71419c583f6e84ca1b279e9030fcde498fdeb
wpacket <40580581+wpacket@users.noreply.github.com>
2022-10-07 17:04:13 +0200

    Update tgen-web.sh

--- tgen-web.sh
+++ tgen-web.sh
@@ -12,6 +12,10 @@ wget -O /dev/null -q https://web.webex.com/sign-in --no-check-certificate &
 
 wget -O /dev/null -q https://www.facebook.com/ --no-check-certificate &
 wget -O /dev/null -q https://www.instagram.com/ --no-check-certificate &
+wget -O /dev/null -q https://www.linkedin.com --no-check-certificate &
+wget -O /dev/null -q https://www.reddit.com/ --no-check-certificate &
+wget -O /dev/null -q https://telegram.org/ --no-check-certificate &
+
 
 wget -O /dev/null -q https://www.google.fr/maps --no-check-certificate &
 wget -O /dev/null -q https://drive.google.com/drive/my-drive --no-check-certificate &
@@ -23,10 +27,10 @@ wget -O /dev/null -q https://open.spotify.com/ --no-check-certificate &
 wget -O /dev/null -q https://www.theguardian.com/ --no-check-certificate &
 wget -O /dev/null -q https://en.wikipedia.org/ --no-check-certificate &
 
-wget -O /dev/null -q https://secure.eicar.org/eicar.com --no-check-certificate &
-wget -O /dev/null -q https://secure.eicar.org/eicar.com.txt --no-check-certificate &
-wget -O /dev/null -q https://secure.eicar.org/eicar_com.zip --no-check-certificate &
-wget -O /dev/null -q https://secure.eicar.org/eicarcom2.zip --no-check-certificate &
+curl https://secure.eicar.org/eicar.com --insecure -m 2 > /dev/null&
+curl https://secure.eicar.org/eicar.com.txt --insecure -m 2 > /dev/null&
+curl https://secure.eicar.org/eicar_com.zip --insecure -m 2 > /dev/null&
+curl https://secure.eicar.org/eicarcom2.zip --insecure -m 2 > /dev/null&
 
 sleep 3
 
