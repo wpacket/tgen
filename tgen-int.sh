ab93fece64fb736b49cad9b96d40ae96332be7ef
wpacket <40580581+wpacket@users.noreply.github.com>
2022-10-07 10:37:22 +0200

    Update tgen-int.sh

--- tgen-int.sh
+++ tgen-int.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
-
-while true
-do
-
-wget -O /dev/null -q https://www.microsoft.com/fr-fr/microsoft-teams/log-in &
-wget -O /dev/null -q https://www.microsoft.com/en-us/microsoft-365/sharepoint/collaboration &
-wget -O /dev/null -q https://www.office.com/ &
-wget -O /dev/null -q https://login.salesforce.com/?locale=eu &
-wget -O /dev/null -q https://zoom.us/signin#/login &
-wget -O /dev/null -q https://web.webex.com/sign-in &
-
-wget -O /dev/null -q https://www.facebook.com/ &
-wget -O /dev/null -q https://www.instagram.com/ &
-
-wget -O /dev/null -q https://www.google.fr/maps &
-wget -O /dev/null -q https://drive.google.com/drive/my-drive &
-wget -O /dev/null -q https://mail.google.com/ &
-
-wget -O /dev/null -q https://www.youtube.com/ &
-wget -O /dev/null -q https://open.spotify.com/ &
-
-wget -O /dev/null -q https://www.theguardian.com/ &
-wget -O /dev/null -q https://en.wikipedia.org/ &
-
-wget -O /dev/null -q https://secure.eicar.org/eicar.com &
-wget -O /dev/null -q https://secure.eicar.org/eicar.com.txt &
-wget -O /dev/null -q https://secure.eicar.org/eicar_com.zip &
-wget -O /dev/null -q https://secure.eicar.org/eicarcom2.zip &
-
-sleep 3
-
-done
+ping 192.168.10.254 > /dev/null &
+ping 192.168.100.254 > /dev/null &
+ping 192.168.20.254 > /dev/null &
+ping 192.168.200.254 > /dev/null &
