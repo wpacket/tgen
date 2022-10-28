139a67bac039ab6df16597d9bdad2b882c692220
wpacket <40580581+wpacket@users.noreply.github.com>
2022-10-28 09:21:59 +0200

    Update tgen-web.sh

--- tgen-web.sh
+++ tgen-web.sh
@@ -3,6 +3,9 @@
 while true
 do
 
+############
+# Work URL
+############
 curl -s https://www.microsoft.com/fr-fr/microsoft-teams/log-in --insecure -m 2 > /dev/null& 
 curl -s https://www.microsoft.com/en-us/microsoft-365/sharepoint/collaboration --insecure -m 2 > /dev/null& 
 curl -s https://www.office.com/ --insecure -m 2 > /dev/null& 
@@ -10,6 +13,9 @@ curl -s https://login.salesforce.com/?locale=eu --insecure -m 2 > /dev/null&
 curl -s https://zoom.us/signin#/login --insecure -m 2 > /dev/null& 
 curl -s https://web.webex.com/sign-in --insecure -m 2 > /dev/null& 
 
+##################
+# Social Media URL
+##################
 curl -s https://www.facebook.com/ --insecure -m 2 > /dev/null& 
 curl -s https://www.instagram.com/ --insecure -m 2 > /dev/null& 
 curl -s https://www.linkedin.com --insecure -m 2 > /dev/null& 
@@ -17,13 +23,22 @@ curl -s https://www.reddit.com/ --insecure -m 2 > /dev/null&
 curl -s https://telegram.org/ --insecure -m 2 > /dev/null& 
 
 
+##################
+# Search Engine URL
+##################
 curl -s https://www.google.fr/maps --insecure -m 2 > /dev/null& 
 curl -s https://drive.google.com/drive/my-drive --insecure -m 2 > /dev/null& 
 curl -s https://mail.google.com/ --insecure -m 2 > /dev/null& 
 
+##################
+# Streaming URL
+##################
 curl -s https://www.youtube.com/ --insecure -m 2 > /dev/null& 
 curl -s https://open.spotify.com/ --insecure -m 2 > /dev/null& 
 
+##################
+# News URL
+##################
 curl -s https://www.theguardian.com/ --insecure -m 2 > /dev/null& 
 curl -s https://en.wikipedia.org/ --insecure -m 2 > /dev/null& 
 
