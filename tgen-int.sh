b11d3dfc293f5f71d527f4fcaa7e864582a9e761
wpacket <40580581+wpacket@users.noreply.github.com>
2022-11-23 11:08:19 +0100

    Update tgen-int.sh

--- tgen-int.sh
+++ tgen-int.sh
@@ -3,3 +3,9 @@ ping 192.168.10.254 > /dev/null &
 ping 192.168.100.254 > /dev/null &
 ping 192.168.20.254 > /dev/null &
 ping 192.168.200.254 > /dev/null &
+
+ping 10.10.10.254 > /dev/null &
+ping 10.10.20.254 > /dev/null &
+ping 10.10.30.254 > /dev/null &
+ping 10.10.40.254 > /dev/null &
+
