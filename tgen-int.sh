707c6099d1f210fd0b55aa92082867738469f5d3
wpacket <40580581+wpacket@users.noreply.github.com>
2023-03-01 10:19:42 +0100

    Update tgen-int.sh

--- tgen-int.sh
+++ tgen-int.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 ping 192.168.10.254 > /dev/null &
-ping 192.168.100.254 > /dev/null &
 ping 192.168.20.254 > /dev/null &
-ping 192.168.200.254 > /dev/null &
+ping 192.168.30.254 > /dev/null &
 
-ping 10.10.10.254 > /dev/null &
-ping 10.10.20.254 > /dev/null &
-ping 10.10.30.254 > /dev/null &
-ping 10.10.40.254 > /dev/null &
+ping 192.168.11.254 > /dev/null &
+ping 192.168.12.254 > /dev/null &
+ping 192.168.13.254 > /dev/null &
+
+ping 192.168.20.254 > /dev/null &
+ping 192.168.21.254 > /dev/null &
+ping 192.168.22.254 > /dev/null &
+
+
+
 
