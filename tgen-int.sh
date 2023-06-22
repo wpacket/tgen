fffe75d8d596f0dfd55c9faf4c5cb586ed8fe37c
wpacket <40580581+wpacket@users.noreply.github.com>
2023-06-22 15:43:10 +0200

    Update tgen-int.sh

--- tgen-int.sh
+++ tgen-int.sh
@@ -2,21 +2,25 @@
 
 sleep 5
 
-ping 192.168.1.254 > /dev/null &
-ping 192.168.2.254 > /dev/null &
-ping 192.168.3.254 > /dev/null &
+ping 192.168.1.254 -c 2 > /dev/null &
+ping 192.168.2.254 -c 2 > /dev/null &
+ping 192.168.3.254 -c 2 > /dev/null &
 
-ping 192.168.10.254 > /dev/null &
-ping 192.168.20.254 > /dev/null &
-ping 192.168.30.254 > /dev/null &
+ping 192.168.10.254 -c 2 > /dev/null &
+ping 192.168.20.254 -c 2 > /dev/null &
+ping 192.168.30.254 -c 2 > /dev/null &
 
-ping 192.168.11.254 > /dev/null &
-ping 192.168.12.254 > /dev/null &
-ping 192.168.13.254 > /dev/null &
+ping 192.168.10.2 -c 2 > /dev/null &
+ping 192.168.20.2 -c 2 > /dev/null &
+ping 192.168.30.2 -c 2 > /dev/null &
 
-ping 192.168.20.254 > /dev/null &
-ping 192.168.21.254 > /dev/null &
-ping 192.168.22.254 > /dev/null &
+ping 192.168.11.254 -c 2 > /dev/null &
+ping 192.168.12.254 -c 2 > /dev/null &
+ping 192.168.13.254 -c 2 > /dev/null &
+
+ping 192.168.20.254 -c 2 > /dev/null &
+ping 192.168.21.254 -c 2 > /dev/null &
+ping 192.168.22.254 -c 2 > /dev/null &
 
 
 
